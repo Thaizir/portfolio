6aa576d920040bd610851c1c7e04d2d5d84b2af9
thaizire@gmail.com <thaizire@gmail.com>
2024-03-28 15:15:39 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'verde-link': '#1CB698',
+			'gris-fondo': '#799097',
+			'fondo-claro': '#5eead4',
+			'fondo-form': '#252a2e',
+			'color-texto': '#164b43',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
